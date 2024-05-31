69c496ee8adeab798811cb029a32fd69dc3afe82
Paste-github <79767609+Paste-github@users.noreply.github.com>
2024-05-31 12:42:57 +0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,11 +1,4 @@
 #include "Graph.h"
 
-Graph::Graph(int V) {
-    this->V = V;
-    adj.resize(V, std::vector<int>(V, 0));
-}
-
-void Graph::addEdge(int u, int v, int weight) {
-    adj[u][v] = weight;
-    adj[v][u] = weight; // For an undirected graph
-}
+// Constructor definition
+Graph::Graph(const std::vector<std::vector<int>>& adjacencyMatrix) : adj(adjacencyMatrix), V(adjacencyMatrix.size()) {}
